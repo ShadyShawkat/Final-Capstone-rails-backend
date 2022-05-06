7dbaebefd7e170ea15e1004d05597d2f68ee7575
TheLuisAbarca <arclaav.adm@gmail.com>
2022-05-06 14:23:49 -0500

    Add Hotels migration files.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_06_174106) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_06_192041) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "hotels", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "rating"
+    t.string "location"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_hotels_on_user_id"
+  end
 
   create_table "jwt_denylist", force: :cascade do |t|
     t.string "jti", null: false
@@ -44,4 +56,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_06_174106) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "hotels", "users"
 end
