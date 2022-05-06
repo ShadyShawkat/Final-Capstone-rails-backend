8519ad448a2e2d273652796569b5bdf0c2fb92ff
mre55 <misganawmihreteab>
2022-05-06 23:00:21 +0300

    Add hotel rooms table with image and price column

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_06_192041) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_06_195917) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "hotel_rooms", force: :cascade do |t|
+    t.string "image"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "hotels", force: :cascade do |t|
     t.string "name"
