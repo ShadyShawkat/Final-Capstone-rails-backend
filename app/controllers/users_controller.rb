0ec4f2185e5182a1dd9375e6bb238471996f9f47
Santiago Velosa <santiagovelosa@gmail.com>
2022-05-06 15:22:24 -0500

    Set up users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show update destroy]
+  before_action :authenticate_user!
 
   # GET /users
   def index
@@ -47,6 +48,6 @@ class UsersController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def user_params
-    params.require(:user).permit(:name, :admin, :profile_picture)
+    params.require(:user).permit(:name, :profile_picture)
   end
 end
