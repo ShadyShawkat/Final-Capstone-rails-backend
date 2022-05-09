0a58acf15c10289447cd5d80c00e765ebfbca369
TheLuisAbarca <arclaav.adm@gmail.com>
2022-05-09 11:28:07 -0500

    Fix Index Action to show all.

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,17 +1,6 @@
 class RoomsController < ApplicationController
-  #before_action :set_room, only: [:index]
   def index
-    rooms = Room.where(hotel_id: params[:hotel_id])
+    rooms = Room.all
     render json: rooms
-  end
-
-  private
-  
-  #def set_room
-  #  @room = Room.find(params[:id])
-  #end
-
-  def room_params
-    params.require(:room).permit(:type, :guests_number)
   end
 end
