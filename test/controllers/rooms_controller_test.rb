035adbf5366aa09f50e2341f179512955d85a194
TheLuisAbarca <arclaav.adm@gmail.com>
2022-05-09 11:29:15 -0500

    Fix Rubocop Linters.

--- test/controllers/rooms_controller_test.rb
+++ test/controllers/rooms_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class RoomsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get rooms_index_url
     assert_response :success
   end
