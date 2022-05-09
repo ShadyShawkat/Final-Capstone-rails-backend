1dbb176519414ee63598071619c7b6965334291b
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2022-05-09 23:29:30 +0200

    Fix destroy method for reservation controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,38 +1,33 @@
 class ReservationsController < ApplicationController
-  before_action :set_reservations, only: %i[create destroy]
-
+  before_action :set_reservations, only: %i[destroy]
+  before_action :reservations_params, only: %i[create]
+  
   # GET /reservations
   def index
-    @reservations = Reservation.where(user_id => @current_user.id)
-
-    render json: @reservations
+    reservations = Reservation.where(user_id: @current_user.id)
+    render json: reservations
   end
-
   # POST /reservations
   def create
-    @reservations = Reservation.new(reservations_params)
-
+    @reservations = @current_user.reservations.new(reservations_params)
     if @reservations.save
       render json: @reservations, status: :created, location: @reservations
     else
       render json: @reservations.errors, status: :unprocessable_entity
     end
   end
-
   # DELETE /reservations/1
   def destroy
-    @reservations.destroy
+    @reservation.destroy
+    render json: "Reservation deleted successfully", status: 200
   end
-
   private
-
   # Use callbacks to share common setup or constraints between actions.
   def set_reservations
-    @reservations = Reservation.find(params[:id])
+    @reservation = Reservation.find(params[:id])
   end
-
   # Only allow a list of trusted parameters through.
   def reservations_params
-    params.require(:reservation).permit(:date, :user_id, :hotel_room_id)
+    params.require(:reservation).permit(:date, :hotel_room_id)
   end
 end
