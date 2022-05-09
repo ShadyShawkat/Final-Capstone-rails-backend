3c3754a9ca947b112455fe2a3ed09c8f08e711fb
mre55 <misganawmihreteab>
2022-05-09 19:29:25 +0300

    Fix linters

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,5 +1,5 @@
 class ReservationsController < ApplicationController
-  before_action :set_reservations, only: %i[ index create destroy ]
+  before_action :set_reservations, only: %i[index create destroy]
   before_action :authenticate_user!
 
   # GET /reservations
@@ -26,13 +26,14 @@ class ReservationsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_reservations
-      @reservations = Reservation.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def reservations_params
-      params.require(:reservation).permit(:date, :user_id, :hotel_room_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_reservations
+    @reservations = Reservation.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def reservations_params
+    params.require(:reservation).permit(:date, :user_id, :hotel_room_id)
+  end
 end
--- test/controllers/reservations_controller_test.rb
+++ test/controllers/reservations_controller_test.rb
@@ -1,35 +1,35 @@
-require "test_helper"
+require 'test_helper'
 
 class ReservationsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @reservations = reservations(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get reservations_url, as: :json
     assert_response :success
   end
 
-  test "should create reservations" do
-    assert_difference("ReservationsController.count") do
-      post reservations_url, params: { reservations: {  } }, as: :json
+  test 'should create reservations' do
+    assert_difference('ReservationsController.count') do
+      post reservations_url, params: { reservations: {} }, as: :json
     end
 
     assert_response :created
   end
 
-  test "should show reservations" do
+  test 'should show reservations' do
     get reservations_url(@reservations), as: :json
     assert_response :success
   end
 
-  test "should update reservations" do
-    patch reservations_url(@reservations), params: { reservations: {  } }, as: :json
+  test 'should update reservations' do
+    patch reservations_url(@reservations), params: { reservations: {} }, as: :json
     assert_response :success
   end
 
-  test "should destroy reservations" do
-    assert_difference("ReservationsController.count", -1) do
+  test 'should destroy reservations' do
+    assert_difference('ReservationsController.count', -1) do
       delete reservations_url(@reservations), as: :json
     end
 
