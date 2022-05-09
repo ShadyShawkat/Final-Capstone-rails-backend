0f06634ac6bf31db55faed12379bd040f04c485a
TheLuisAbarca <arclaav.adm@gmail.com>
2022-05-09 11:13:50 -0500

    Add Index action to the controller of Rooms.

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,17 @@
 class RoomsController < ApplicationController
+  #before_action :set_room, only: [:index]
   def index
+    rooms = Room.where(hotel_id: params[:hotel_id])
+    render json: rooms
+  end
+
+  private
+  
+  #def set_room
+  #  @room = Room.find(params[:id])
+  #end
+
+  def room_params
+    params.require(:room).permit(:type, :guests_number)
   end
 end
