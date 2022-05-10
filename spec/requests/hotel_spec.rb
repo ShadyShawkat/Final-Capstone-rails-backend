9a61d41128fbc9186236c1c893ec8f58dcd0adc9
mre55 <misganawmihreteab>
2022-05-10 21:04:00 +0300

    Add test to check list of hotels created

--- spec/requests/hotel_spec.rb
+++ spec/requests/hotel_spec.rb
@@ -6,10 +6,16 @@ RSpec.describe 'Hotels', type: :request do
     post '/auth/login', params: { email: @admin.email, password: "admin" }
     @token = JSON.parse(response.body)['token']
   end
-  
+
   it "returns a 200" do
     get '/hotels', params: {}, headers: { Authorization: "Bearer #{@token}" }
     expect(response).to have_http_status(:ok)
+  end
+
+  it "returns a list of hotels" do
+    get '/hotels', params: {}, headers: { Authorization: "Bearer #{@token}" }
+    hotels = JSON.parse(response.body)
+    expect(hotels.length).to be(2)
   end
 
 end
