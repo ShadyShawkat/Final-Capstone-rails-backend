76639c70cc9883b0ae5023c7624055fd0c169bba
mre55 <misganawmihreteab>
2022-05-10 21:03:24 +0300

    Add test to check creatation is succesful

--- spec/requests/hotel_spec.rb
+++ spec/requests/hotel_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Hotels', type: :request do
+  before(:each) do
+    @admin = User.find_by(admin: true)
+    post '/auth/login', params: { email: @admin.email, password: "admin" }
+    @token = JSON.parse(response.body)['token']
+  end
   
+  it "returns a 200" do
+    get '/hotels', params: {}, headers: { Authorization: "Bearer #{@token}" }
+    expect(response).to have_http_status(:ok)
+  end
+
 end
