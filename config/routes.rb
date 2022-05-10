1c23dc8d6d896672b553745774f47d5e2969d668
ShadyShawkat <shadyshawkat91@gmail.com>
2022-05-10 21:58:24 +0200

    Update routes and root

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :reservations, only: [:index, :create, :destroy]
-  resources :hotels, only: [:index, :show, :create, :destroy]
   post "/auth/login", to: "authentication#login"
 
+  resources :users
+  resources :reservations, only: [:index, :create, :destroy]
+  resources :rooms, only: [:index]
+  resources :hotels, only: [:index, :show, :create, :destroy]
 
-  get 'rooms/index'
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "hotels#index"
 end
