df2948e3bc444e5d725699294e06a0eaafe40a40
Santiago Velosa <santiagovelosa@gmail.com>
2022-05-10 11:54:02 -0500

    Fix linters

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Users controller', type: :request do
   before do
-    post users_path({"name": "Whatever", "email": "a@gmail.com", "password": "123456"})
+    post users_path({ name: 'Whatever', email: 'a@gmail.com', password: '123456' })
   end
 
   describe 'POST /users' do
@@ -13,11 +13,11 @@ RSpec.describe 'Users controller', type: :request do
 
   describe 'POST /auth/login' do
     before do
-      post auth_login_path({"email": "a@gmail.com", "password": "123456"})
+      post auth_login_path({ email: 'a@gmail.com', password: '123456' })
     end
 
     it 'logs in an existent user successfully' do
-      expect(JSON.parse(response.body)).to have_key("token")
+      expect(JSON.parse(response.body)).to have_key('token')
     end
 
     it 'returns a 200 status' do
@@ -27,9 +27,10 @@ RSpec.describe 'Users controller', type: :request do
 
   describe 'DELETE users/:id' do
     before do
-      post auth_login_path({"email": "a@gmail.com", "password": "123456"})
-      @token = JSON.parse(response.body)["token"]
-      delete '/users/3', params: {"email": "a@gmail.com", "password": "123456"}, headers: {Authorization: "Bearer #{@token}"}
+      post auth_login_path({ email: 'a@gmail.com', password: '123456' })
+      @token = JSON.parse(response.body)['token']
+      delete '/users/3', params: { email: 'a@gmail.com', password: '123456' },
+                         headers: { Authorization: "Bearer #{@token}" }
     end
 
     it 'deletes a user successfully' do
