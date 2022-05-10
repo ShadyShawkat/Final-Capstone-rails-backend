61e00c932e6f6786600efb7ebdf86dbfe1126389
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2022-05-10 18:55:59 +0200

    Add test for index method of reservation controller

---
+++ spec/requests/reservation_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "reservations_controller", :type => :request do
+  before :each do
+    post '/auth/login', params: { email: "admin@gmail.com", password: "admin" }
+    @token = JSON.parse(response.body)['token']
+  end
+
+  it "GET index" do
+    get "/reservations", params: {}, headers: { Authorization: "Bearer #{@token}" }
+    # hotels = JSON.parse(response.body)
+    expect(response).to have_http_status(:ok)
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,6 +28,10 @@ RSpec.configure do |config|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
+  config.before(:suite) do
+    Rails.application.load_seed # loading seeds
+  end
+
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
