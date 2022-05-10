d09c88e54c532b46fd0962dd10096607e02ad0fe
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2022-05-11 01:36:25 +0200

    Edit the default host

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -22,10 +22,10 @@ RSpec.configure do |config|
       paths: {},
       servers: [
         {
-          url: 'https://{defaultHost}',
+          url: 'http://{defaultHost}',
           variables: {
             defaultHost: {
-              default: 'www.example.com'
+              default: '127.0.0.1:3000'
             }
           }
         }
