63d861d525d70418999b33e69e9b1b5d6f4862e2
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2022-05-12 15:49:36 +0200

    Fix hotels api tests

--- spec/requests/api/hotels_spec.rb
+++ spec/requests/api/hotels_spec.rb
@@ -1,11 +1,20 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/hotels', type: :request do
+  before :each do
+    post '/auth/login', params: { email: 'admin@gmail.com', password: 'admin' }
+    @token = JSON.parse(response.body)['token']
+  end
+
   path '/hotels' do
     get('list hotels') do
+      tags 'Hotels'
       consumes 'application/json'
       produces 'application/json'
+      security [Bearer: {}]
+      parameter name: :Authorization, in: :header, type: :string, required: true, description: 'Client token'
       response(200, 'successful') do
+        let(:Authorization) { 'Bearer ' + @token }
         run_test!
       end
     end
@@ -14,19 +23,20 @@ RSpec.describe 'api/hotels', type: :request do
       tags 'Hotels'
       consumes 'application/json'
       produces 'application/json'
-      security [bearer_auth: []]
-      parameter name: :Authorization, in: :header, type: :string
+      security [Bearer: {}]
+      parameter name: :Authorization, in: :header, type: :string, required: true, description: 'Client token'
       parameter name: :hotel, in: :body, schema: {
         type: :object,
         properties: { name: { type: :string }, description: { type: :text }, rating: { type: :integer },
-                      image: { type: :string }, location: { type: :string }, user_id: { type: :string } },
+                      image: { type: :string }, location: { type: :string }, user_id: { type: :integer },rooms: { type: :multi } },
         required: %w[name description rating image location user_id]
       }
 
       response '201', 'hotel created' do
+        let(:Authorization) { 'Bearer ' + @token }
         let(:hotel) do
           { name: 'Hilto', description: 'Nice place.', rating: 5, image: 'Hilton hotel image', location: 'New York',
-            user_id: '2' }
+            user_id: 2, rooms: [{id: 1, image: 'image', price: 1000}] }
         end
         run_test!
       end
@@ -38,10 +48,12 @@ RSpec.describe 'api/hotels', type: :request do
       tags 'Hotels'
       consumes 'application/json'
       produces 'application/json'
-      security [bearer_auth: []]
-      parameter name: :Authorization, in: :header, type: :string
+      security [Bearer: {}]
+      parameter name: :Authorization, in: :header, type: :string, required: true, description: 'Client token'
       parameter name: :id, in: :path, type: :integer
       response(200, 'successful') do
+        let(:Authorization) { 'Bearer ' + @token }
+        let(:id) { 1 }
         run_test!
       end
     end
@@ -50,10 +62,12 @@ RSpec.describe 'api/hotels', type: :request do
       tags 'Hotels'
       consumes 'application/json'
       produces 'application/json'
-      security [bearer_auth: []]
-      parameter name: :Authorization, in: :header, type: :string
+      security [Bearer: {}]
+      parameter name: :Authorization, in: :header, type: :string, required: true, description: 'Client token'
       parameter name: :id, in: :path, type: :integer
       response(200, 'successful') do
+        let(:Authorization) { 'Bearer ' + @token }
+        let(:id) { 1 }
         run_test!
       end
     end
